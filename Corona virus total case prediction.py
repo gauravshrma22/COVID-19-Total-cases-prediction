0e5326b46e2f38fe58937132e4f641cecbbc09c0
Gaurav Sharma <60601706+gauravshrma22@users.noreply.github.com>
2020-04-28 15:44:17 +0530

    Add files via upload

---
+++ Corona virus total case prediction.py
@@ -0,0 +1,47 @@
+# Corona Virus Epedemic
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('total_cases.csv')
+
+X = dataset.iloc[85:,0:1].values
+y = dataset.iloc[85:,2].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(X, y)
+
+# Fitting Polynomial Regression to the dataset
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg = PolynomialFeatures(degree = 2)
+X_poly = poly_reg.fit_transform(X)
+poly_reg.fit(X_poly, y)
+lin_reg_2 = LinearRegression()
+lin_reg_2.fit(X_poly, y)
+
+# Visualising the Linear Regression results
+plt.scatter(X, y, color = 'red')
+plt.plot(X, lin_reg.predict(X), color = 'blue')
+plt.title('Coronavirus Total Cases')
+plt.xlabel('No of days')
+plt.ylabel('Total Cases')
+plt.show()
+
+# Visualising the Polynomial Regression results
+plt.scatter(X, y, color = 'red')
+plt.plot(X, lin_reg_2.predict(poly_reg.fit_transform(X)), color = 'blue')
+plt.title('Coronavirus Total Cases')
+plt.xlabel('No of days')
+plt.ylabel('Total Cases')
+plt.show()
+
+lin_reg.predict([[120]])
+
+lin_reg_2.predict(poly_reg.fit_transform([[192]]))
